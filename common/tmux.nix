3aaf6d3da06378dc4400e590d4036a451c05c1f3
teeaychem <ben@sparkes.uk.net>
2025-11-22 00:28:17 +0000

    update tmux/resurrect-dir

--- common/tmux.nix
+++ common/tmux.nix
@@ -22,6 +22,7 @@
         extraConfig = ''
           set -g @resurrect-strategy-nvim 'session'
           set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-dir '$XDG_CACHE_HOME/tmux'
         '';
       }
       {
