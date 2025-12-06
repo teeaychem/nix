4eaba63f06c8b5a05cf71d359adda144c28f7735
teeaychem <ben@sparkes.uk.net>
2025-12-06 19:00:14 +0000

    nix: flake tidying
    more system config to ./darwin/darwin.nix
    rename ./darwin/default.nix to ./darwin/home.nix for clarity
    
    as `self` because unavailable in the darwin.nix move, configurationRevision is now set to null.
    
    no other changes

---
+++ darwin/darwin.nix
@@ -0,0 +1,122 @@
+{ config, pkgs, ... }:
+{
+  # $ nix-env -qaP
+  nix.enable = false;
+
+  environment = {
+    shells = with pkgs; [
+      bash
+      fish
+      zsh
+    ];
+
+    systemPackages = with pkgs; [
+      bat
+      coreutils
+      delta
+      fd
+      fzf
+      hunspell
+      imagemagick
+      neovim
+      nix-your-shell
+      nixfmt-rfc-style
+      pkg-configUpstream # ?pkg-config fails
+      ripgrep
+      starship
+      tmux
+      typos
+      zellij
+      zoxide
+
+      tree-sitter
+      tree-sitter-grammars.tree-sitter-bash
+      tree-sitter-grammars.tree-sitter-c
+      tree-sitter-grammars.tree-sitter-cpp
+      tree-sitter-grammars.tree-sitter-elisp
+      tree-sitter-grammars.tree-sitter-fish
+      tree-sitter-grammars.tree-sitter-haskell
+      tree-sitter-grammars.tree-sitter-javascript
+      tree-sitter-grammars.tree-sitter-json
+      tree-sitter-grammars.tree-sitter-latex
+      tree-sitter-grammars.tree-sitter-lua
+      tree-sitter-grammars.tree-sitter-nix
+      tree-sitter-grammars.tree-sitter-ocaml
+      tree-sitter-grammars.tree-sitter-python
+      tree-sitter-grammars.tree-sitter-rust
+      tree-sitter-grammars.tree-sitter-scheme
+      tree-sitter-grammars.tree-sitter-toml
+      tree-sitter-grammars.tree-sitter-vim
+
+    ];
+  };
+
+  fonts.packages = [
+    pkgs.nerd-fonts."m+"
+  ];
+
+  nix.settings.experimental-features = "nix-command flakes";
+
+  system = {
+    configurationRevision = null;
+    stateVersion = 6;
+    primaryUser = "sparkes";
+
+    defaults = {
+      dock.autohide = true;
+      NSGlobalDomain = {
+        AppleFontSmoothing = 0;
+        AppleICUForce24HourTime = true;
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        AppleShowScrollBars = "Always";
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticWindowAnimationsEnabled = false;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSNavPanelExpandedStateForSaveMode2 = true;
+      };
+    };
+
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+  };
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    # taps = [ "d12frosted/emacs-plus" ];
+    brews = [
+      # "emacs-plus@30"
+    ];
+    casks = [
+      "doll"
+      "hammerspoon"
+      "raycast"
+      "rectangle"
+      "skim"
+      "xld"
+      "vlc"
+    ];
+    caskArgs.no_quarantine = true;
+  };
+
+  programs.fish.enable = true;
+
+  users = {
+    knownUsers = [ config.system.primaryUser ];
+    users.${config.system.primaryUser} = {
+      uid = 501;
+      shell = pkgs.fish;
+    };
+  };
+}
